4af5f422d5e7f6046a528dbfea24248613deec3c
mehlearning <111727343+mehlearning@users.noreply.github.com>
2022-09-14 23:20:48 -0700

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,27 @@
+const Data = [
+           {
+           'quote': 'When 900 years old, you reach… Look as good, you will not.','author': '- Yoda'
+         }, {
+           'quote': 'The Dark Side of the Force is the pathway to many abilities some consider to be.. Unnatural.','author': '- Senator palpatine'
+         }, {
+           'quote': 'I sense great fear in you, Skywalker. You have hate… you have anger… but you don’t use them.', 'author': '- Count Dooku'
+         }, {
+           'quote': 'Luke, you can destroy the Emperor. He has foreseen this. It is your destiny. Join me, and together we can rule the galaxy as father and son.', 'author': '- Darth Vader'
+         }, {
+           'quote': 'Aren\'t you a little short for a storm trooper?', 'author': '- Princess Leia'
+         }, {
+           'quote': 'Mmm. Lost a planet, Master Obi-Wan has. How embarrassing.', 'author': '- Yoda'
+         }, {
+           'quote': 'Your focus determines your reality', 'author': '- Qui-Gon Jinn'
+         }, {
+           'quote': 'Do. Or do not. There is no try.', 'author': '- Yoda'
+         }, {
+           'quote': 'In my experience there is no such thing as luck.', 'author': '- Obi-Wan-Kenobi'
+         }, {
+           'quote': 'If once you start down the dark side, forever will it dominate your destiney, consume you it will, as it did Obi-Wan\'s apprentice', 'author': '- Yoda'
+         }, {
+           'quote': 'The Force is strong with this one.', 'author': '- Darth Vader'
+         }
+       ]
+
+export default Data;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App'
+import List from "./List"
+
+ReactDOM.render(
+  <>
+    <App />
+    <List />
+  </>,
+  document.getElementById('root')
+);
+
