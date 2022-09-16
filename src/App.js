44378c0129571713b529b277d307dfc9064f6068
mehlearning <111727343+mehlearning@users.noreply.github.com>
2022-09-15 17:17:08 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
   const getQuotes = () => {
     fetch(
-      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"  
+      "https://gist.githubusercontent.com/carmandomx/3d7ac5f15af87a587e1d25f5ba96de61/raw/e2d848b87c730a580077de221666343c15b1a243/gistfile1.txt"  
     )
       .then((res) => res.json())
       .then((data) => {
